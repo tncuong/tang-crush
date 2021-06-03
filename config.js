019a65060d887b75a84fd7e559860e7a2a981767
tncuong <80331129+tncuong@users.noreply.github.com>
2021-06-03 16:24:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,5 +14,5 @@ const CONFIG = {
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: :/https://www.facebook.com/messages/t/100054258341605
 }
